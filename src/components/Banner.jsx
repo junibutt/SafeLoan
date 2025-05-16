ab8df3cf8665eb986ddd04fbb33f6f43655964d9
Muhammad Junaid <junnedbutt@gmail.com>
2025-05-16 03:54:26 -0700

    pushing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import girl from "../assets/girl.png";
+import { useModal } from "./ModalContext";
 
 const Banner = () => {
+  const{setOpen}=useModal();
   return (
     <div className="w-full "id="home" >
       <div className="bg-green-700 text-white py-16 px-6 md:px-20">
@@ -16,7 +18,7 @@ const Banner = () => {
               Get the fund you need quickly and securely. Check your loan
               options today with no impact on your credit score.
             </p>
-            <button className="bg-white text-green-700 px-6 py-2 font-semibold rounded shadow cursor-pointer hover:text-green-800">
+            <button onClick={()=>setOpen(true)} className="bg-white text-green-700 px-6 py-2 font-semibold rounded shadow cursor-pointer hover:text-green-800">
               GET STARTED
             </button>
           </div>
